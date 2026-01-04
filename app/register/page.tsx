79001f6ddda1e78ad00d454c5aeedb0d00a2d62b
MuhammadAneerZaman <aneerzaman1@gmail.com>
2026-01-05 03:33:30 +0500

    Add Signup and Login Page

---
+++ app/register/page.tsx
@@ -0,0 +1,65 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { BorderBeam } from "@/components/ui/border-beam"; 
+import { HyperText } from "@/components/ui/hyper-text"
+import { useRouter } from "next/navigation"
+
+function RegisterForm() {
+  const router = useRouter()
+  return (
+    <div className=" h-screen w-screen flex justify-center items-center" >
+    <Card className="relative w-87.5 overflow-hidden">
+      <CardHeader>
+        <CardTitle><HyperText>Rgister</HyperText></CardTitle>
+        <CardDescription>
+          Enter required credentials to register yourself.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Name</Label>
+              <Input id="name" type="email" placeholder="Enter your Name" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="Enter your email" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button onClick={()=>{ router.push('/login') }} className="cursor-pointer" variant="outline">Login</Button>
+        <Button className="cursor-pointer">Register</Button>
+      </CardFooter>
+      <BorderBeam
+        duration={4}
+        size={300}
+        reverse
+        className="from-transparent via-green-500 to-transparent"
+      />
+    </Card>
+    </div>
+  );
+}
+
+export default RegisterForm;
